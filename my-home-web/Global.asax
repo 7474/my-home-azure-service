1c2923721f147dfce4f2118f92d86ef7fa72ca99
koudenpa <koudenpa@hotmail.com>
2016-09-07 23:39:33 +0900

    Add project files.

---
+++ my-home-web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="my_home_web.MvcApplication" Language="C#" %>
---
+++ my-home-web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(my_home_web.Startup))]
+namespace my_home_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
